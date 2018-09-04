7910ea79a254b29a024919fd77c01e7abc2ea479
snobw <fballand.inope@gmail.com>
2018-09-04 18:29:11 +0200

    simplification
    orientation sur modele d'horloge

--- allresult.py
+++ allresult.py
@@ -27,32 +27,28 @@ def main():
     """ Main function
     """
 
-    MaxNumber=100
-    currentDigit=0
-    vecteur=[0,0,0]
-    vecteurMem=list(vecteur)
-    limitMaxNumber=len(vecteur)
-    
-    print(limitMaxNumber)
+    MaxNumber = 100
+    vecteur = [0, 0, 0]
+    limitMaxNumber = len(vecteur)
 
-    while (sum(vecteur)<=(limitMaxNumber*MaxNumber)):
-        i=0
-        while (i<MaxNumber):
+    while (sum(vecteur) <= (limitMaxNumber * MaxNumber)):
+        i = 0
+        vecteur[0]=0
+        while(i <MaxNumber):
             i+=1
-            vecteur[currentDigit]=i
-            if(sum(vecteur)==100):
+            vecteur[0]=i
+            if (sum(vecteur) == MaxNumber):
+                print("okkkk")
                 print(vecteur)
 
-        if(currentDigit==limitMaxNumber-1):
-            f=0
-            while(f<=currentDigit):
-                vecteurMem[f]=vecteurMem[f]+1
-                
-                f+=1
-            currentDigit=0
-        else:
-            currentDigit+=1
-        vecteur = list(vecteurMem)
+
+        if(vecteur[0]==MaxNumber):
+            for j in range (1,limitMaxNumber):
+                if((vecteur[j-1]==MaxNumber)):
+                    vecteur[j-1] =0
+                    vecteur[j]=vecteur[j]+1
+
+
     exit(0)
     
 if __name__ == '__main__':
